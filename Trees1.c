0a163aef29132d7909441fda859537b83f5c6575
Pankaj Jadwal <pankajjadwal@gmail.com>
2026-03-23 12:07:12 +0530

    get max, min, count and height

--- Trees1.c
+++ Trees1.c
@@ -46,17 +46,73 @@ void postorder(node *root)
     postorder(root->right);
     printf("%d  ", root->data);
 }
-int main()
+int getmax(node* root)
 {
-    node *root = init(10);
-    root->left = init(20);
-    root->right = init(30);
-    root->left->left = init(40);
-    root->right->right = init(50);
+    if(root==NULL)
+    {
+        return -1e9;
+    }
+    int l=getmax(root->left);
+    int r=getmax(root->right);
+    int ans=(root->data> l && root->data>r)?root->data:(l>r?l:r);
+    return ans;
+}
+int getcount(node* root)
+{
+    if(root==NULL)
+    {
+        return 0;
+    }
+    return getcount(root->left)+getcount(root->right)+1;
+}
+int maxx(int a, int b)
+{
+    return a>b?a:b;
+}
+int getheight(node* root)
+{
+    if(root==NULL)
+    {
+        return 0;
+    }
+    return maxx(getcount(root->left),getcount(root->right))+1;
+}
+int getcountnodes(node* root)
+{
+    if(root==NULL)  return 0;
+
+    return getcountnodes(root->left)+getcountnodes(root->right)+root->data;
+}
+int main()
+{   
+    // node *root = init(10);
+    // root->left = init(20);
+    // root->right = init(30);
+    // root->left->left = init(40);
+    // root->right->right = init(50);
+
+    node* root=init(10);
+    node* twenty=init(20);
+    node* thirty=init(30);
+    node* forty=init(40);
+    node* fifty=init(50);
+    root->left=twenty;
+    root->right=thirty;
+    root->left->left=forty;
+    root->right->right=fifty;
+
     preorder(root);
     printf("\n");
     inorder(root);
     printf("\n");
     postorder(root);
     printf("\n");
+    printf("Max among nodes--> %d\n  ",getmax(root));
+    printf("Number of  nodes--> %d\n  ",getcount(root));
+    printf("Height of the tree --> %d\n  ",getheight(root));
+    printf("count of  all the nodes--> %d\n  ",getcountnodes(root));
+
+
+
+
 }
